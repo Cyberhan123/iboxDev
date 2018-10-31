285bb21e34e8f73c6235b1ca5db58138ba756995
Cyberhan123 <255542417@qq.COM>
2018-10-31 14:45:13 +0800

    init

---
+++ iboxDev/src/main/java/cn/hselfweb/ibox/ibox/IboxApplication.java
@@ -0,0 +1,12 @@
+package cn.hselfweb.ibox.ibox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IboxApplication.class, args);
+    }
+}
